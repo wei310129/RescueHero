613313cdd41bb98b68e4eaf1246561435ca45f0b
AidenChen <wei310129@gmail.com>
2025-10-02 09:40:31 +0800

    initialize

---
+++ src/main/java/tw/com/aidenmade/rescuehero/RescueHeroApplication.java
@@ -0,0 +1,13 @@
+package tw.com.aidenmade.rescuehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueHeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RescueHeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/com/aidenmade/rescuehero/RescueHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.aidenmade.rescuehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueHeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
