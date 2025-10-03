31c86918be86a77d2fb7038061e9f03bc6b94246
AidenChen <wei310129@gmail.com>
2025-10-03 14:39:46 +0800

    feat. 新增 time zone 相關操作
    Signed-off-by: AidenChen <wei310129@gmail.com>

---
+++ src/main/java/tw/com/aidenmade/rescuehero/utils/DateTimeUtils.java
@@ -0,0 +1,17 @@
+package tw.com.aidenmade.rescuehero.utils;
+
+import tw.com.aidenmade.rescuehero.context.TimezoneContext;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+public class DateTimeUtils {
+
+    public DateTimeUtils() {
+        throw new AssertionError("No DateTimeUtils instances for you!");
+    }
+
+    public static ZonedDateTime convertInstantToZonedDateTime(Instant instant) {
+        return instant == null ? null : ZonedDateTime.ofInstant(instant, TimezoneContext.getZone());
+    }
+}
