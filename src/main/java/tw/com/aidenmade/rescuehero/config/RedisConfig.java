331c40e552f7f7db287cda91ec66d2f301e5907e
AidenChen <wei310129@gmail.com>
2025-10-08 16:30:55 +0800

    modify 修正 jasypt 解密碼時機晚於 postgres, redis 連線問題
    Signed-off-by: AidenChen <wei310129@gmail.com>

---
+++ src/main/java/tw/com/aidenmade/rescuehero/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package tw.com.aidenmade.rescuehero.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Slf4j
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setUsername("default");
+        config.setPassword(password); // 此時 Jasypt 已解密完畢
+        return new LettuceConnectionFactory(config);
+    }
+}
