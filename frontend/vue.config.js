ecaadf0e2a6de3545a08e608d63e961648dc74a0
AidenChen <wei310129@gmail.com>
2025-10-15 12:42:40 +0800

    feat. 自訂憑證
    Signed-off-by: AidenChen <wei310129@gmail.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
+const fs = require('fs');
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  devServer: {
+    // HTTPS 憑證：
+    https: {
+      key: fs.readFileSync('./cert/localhost+1-key.pem'),
+      cert: fs.readFileSync('./cert/localhost+1.pem')
+    },
+    port: 8080,
+  }
+}
