b44ad1f498dbe8c16feda8aa60ade163b53d105d
AidenChen <wei310129@gmail.com>
2025-10-15 12:43:41 +0800

    feat. 本機開發時關閉冗餘WebSocket
    Signed-off-by: AidenChen <wei310129@gmail.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,5 +9,21 @@ module.exports = {
       cert: fs.readFileSync('./cert/localhost+1.pem')
     },
     port: 8080,
+
+    // 關掉 HMR（Hot Module Replacement）
+    hot: false,
+    // 關掉 Live Reload（自動刷新頁面）
+    liveReload: false,
+    // 關掉錯誤畫面 overlay
+    client: {
+      overlay: false,
+      progress: false, // 不顯示 console 的進度條
+      webSocketURL: {
+        protocol: 'wss', // 你如果有 HTTPS，可留 wss；否則可改成 ws
+        hostname: '0.0.0.0', // 或 localhost，也可改你的 IP
+        port: 8080,
+        pathname: undefined // 不指定 /ws，避免衝突
+      }
+    }
   }
 }
