8c12abad9a4cf6e88a8e7c456101ac6360c38856
AidenChen <wei310129@gmail.com>
2025-10-15 11:02:38 +0800

    feat. 初始化 frontend 專案
    Signed-off-by: AidenChen <wei310129@gmail.com>

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">切换消息</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const message = ref('你好，Vue 3 + Composition API！')
+
+function changeMessage() {
+  message.value = '消息已切换！'
+}
+</script>
