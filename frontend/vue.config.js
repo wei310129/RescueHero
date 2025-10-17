82142f9f9474d841ac9b734e0455bdfe1234afa8
AidenChen <wei310129@gmail.com>
2025-10-17 16:00:43 +0800

    feat. docker 加入 nginx https 反向代理
    Signed-off-by: AidenChen <wei310129@gmail.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
+// const fs = require('fs');
 const { defineConfig } = require('@vue/cli-service')
 // vue.config.js
 module.exports = {
   devServer: {
     // HTTPS 憑證：
-    https: {
-      key: fs.readFileSync('./cert/localhost+1-key.pem'),
-      cert: fs.readFileSync('./cert/localhost+1.pem')
-    },
+    // https: {
+    //   key: fs.readFileSync('./cert/localhost+1-key.pem'),
+    //   cert: fs.readFileSync('./cert/localhost+1.pem')
+    // },
     port: 8080,
 
     // 關掉 HMR（Hot Module Replacement）
