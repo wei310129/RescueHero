dbff40d2d48f0c542fc9e9bf6178bc3e2515f19a
AidenChen <wei310129@gmail.com>
2025-10-29 15:02:21 +0800

    feat.(back) 新建災害disaster相關基礎
    Signed-off-by: AidenChen <wei310129@gmail.com>

--- src/main/java/tw/com/aidenmade/rescuehero/domain/disaster/mapstruct/DisasterProjectionMapper.java
+++ src/main/java/tw/com/aidenmade/rescuehero/domain/disaster/mapstruct/DisasterProjectionMapper.java
@@ -2,15 +2,14 @@ package tw.com.aidenmade.rescuehero.domain.disaster.mapstruct;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import tw.com.aidenmade.rescuehero.domain.address.mapstruct.AddressProjectionMapper;
-import tw.com.aidenmade.rescuehero.domain.common.mapstruct.AuditInfoProjectionMapper;
-import tw.com.aidenmade.rescuehero.domain.common.mapstruct.CountryProjectionMapper;
+import tw.com.aidenmade.rescuehero.domain.address.mapstruct.AddressMapper;
+import tw.com.aidenmade.rescuehero.domain.base.mapstruct.projection.AuditInfoProjectionMapper;
+import tw.com.aidenmade.rescuehero.domain.base.mapstruct.projection.CountryProjectionMapper;
 import tw.com.aidenmade.rescuehero.domain.disaster.application.dto.DisasterDto;
-import tw.com.aidenmade.rescuehero.domain.common.mapstruct.BaseProjectionMapper;
 import tw.com.aidenmade.rescuehero.domain.disaster.projection.DisasterProjection;
 
-@Mapper(componentModel = "spring", uses = {AuditInfoProjectionMapper.class, CountryProjectionMapper.class, AddressProjectionMapper.class})
-public interface DisasterProjectionMapper extends BaseProjectionMapper {
+@Mapper(componentModel = "spring", uses = {AuditInfoProjectionMapper.class, CountryProjectionMapper.class, AddressMapper.class})
+public interface DisasterProjectionMapper {
     DisasterProjectionMapper INSTANCE = Mappers.getMapper(DisasterProjectionMapper.class);
     DisasterDto toDto(DisasterProjection projection);
 }
--- src/main/java/tw/com/aidenmade/rescuehero/domain/disaster/projection/DisasterProjection.java
+++ src/main/java/tw/com/aidenmade/rescuehero/domain/disaster/projection/DisasterProjection.java
@@ -1,8 +1,8 @@
 package tw.com.aidenmade.rescuehero.domain.disaster.projection;
 
 import tw.com.aidenmade.rescuehero.domain.address.application.dto.AddressDto;
-import tw.com.aidenmade.rescuehero.domain.common.application.dto.AuditInfoDto;
-import tw.com.aidenmade.rescuehero.domain.common.application.dto.CountryDto;
+import tw.com.aidenmade.rescuehero.domain.base.application.dto.AuditInfoDto;
+import tw.com.aidenmade.rescuehero.domain.base.application.dto.CountryDto;
 import tw.com.aidenmade.rescuehero.domain.disaster.enums.DisasterStatus;
 
 import java.time.Instant;
