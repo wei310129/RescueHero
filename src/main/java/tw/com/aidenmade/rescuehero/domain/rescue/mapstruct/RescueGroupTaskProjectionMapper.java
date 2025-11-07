97a8f7da1ff81d1a9c4f3271712bbf4ba5c90c31
AidenChen <wei310129@gmail.com>
2025-11-07 16:30:41 +0800

    fix(back) 修正 RescueGroupTaskProjection 及相關 mapper
    Signed-off-by: AidenChen <wei310129@gmail.com>

--- src/main/java/tw/com/aidenmade/rescuehero/domain/rescue/mapstruct/RescueGroupTaskProjectionMapper.java
+++ src/main/java/tw/com/aidenmade/rescuehero/domain/rescue/mapstruct/RescueGroupTaskProjectionMapper.java
@@ -3,11 +3,12 @@ package tw.com.aidenmade.rescuehero.domain.rescue.mapstruct;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import tw.com.aidenmade.rescuehero.domain.base.mapstruct.projection.AuditInfoProjectionMapper;
+import tw.com.aidenmade.rescuehero.domain.base.mapstruct.projection.StatusProjectionMapper;
 import tw.com.aidenmade.rescuehero.domain.disaster.mapstruct.DisasterProjectionMapper;
 import tw.com.aidenmade.rescuehero.domain.rescue.application.dto.RescueGroupTaskDto;
 import tw.com.aidenmade.rescuehero.domain.rescue.projection.RescueGroupTaskProjection;
 
-@Mapper(componentModel = "spring", uses = {AuditInfoProjectionMapper.class, RescueGroupProjectionMapper.class, DisasterProjectionMapper.class, RescueGroupTaskItemProjectionMapper.class})
+@Mapper(componentModel = "spring", uses = {AuditInfoProjectionMapper.class, RescueGroupProjectionMapper.class, DisasterProjectionMapper.class, StatusProjectionMapper.class, RescueGroupTaskItemProjectionMapper.class})
 public interface RescueGroupTaskProjectionMapper {
     RescueGroupTaskProjectionMapper INSTANCE = Mappers.getMapper(RescueGroupTaskProjectionMapper.class);
     RescueGroupTaskDto toDto(RescueGroupTaskProjection projection);
--- src/main/java/tw/com/aidenmade/rescuehero/domain/rescue/projection/RescueGroupTaskProjection.java
+++ src/main/java/tw/com/aidenmade/rescuehero/domain/rescue/projection/RescueGroupTaskProjection.java
@@ -1,8 +1,10 @@
 package tw.com.aidenmade.rescuehero.domain.rescue.projection;
 
 import tw.com.aidenmade.rescuehero.domain.base.projection.AuditInfoProjection;
+import tw.com.aidenmade.rescuehero.domain.base.projection.StatusProjection;
 import tw.com.aidenmade.rescuehero.domain.disaster.projection.DisasterProjection;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface RescueGroupTaskProjection {
@@ -12,7 +14,11 @@ public interface RescueGroupTaskProjection {
     DisasterProjection getDisaster();
     String getName();
     String getDescription();
+    StatusProjection getStatus();
+    Integer getPriority();
     Integer getMinMember();
     Integer getMaxMember();
+    ZonedDateTime getAssignedAt();
+    ZonedDateTime getCompletedAt();
     List<RescueGroupTaskItemProjection> getItems();
 }
