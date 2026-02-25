76ff29c95a2a6c6f20512dfcc63499e647404741
AidenChen <wei310129@gmail.com>
2026-02-25 17:13:39 +0800

    feat(back) TTLType改用.name()
    Signed-off-by: AidenChen <wei310129@gmail.com>

--- src/main/java/tw/com/aidenmade/rescuehero/enums/TTLType.java
+++ src/main/java/tw/com/aidenmade/rescuehero/enums/TTLType.java
@@ -6,19 +6,18 @@ import java.time.Duration;
 
 @AllArgsConstructor
 public enum TTLType {
-    DAY_7("DAY_7", Duration.ofDays(7)),
-    HOUR_12("HOUR_12", Duration.ofHours(12)),
-    HOUR_1("HOUR_1", Duration.ofHours(1)),
-    MINUTE_5("MINUTE_5", Duration.ofMinutes(5)),
-    MINUTE_1("MINUTE_1", Duration.ofMinutes(1)),
+    DAY_7(Duration.ofDays(7)),
+    HOUR_12(Duration.ofHours(12)),
+    HOUR_1(Duration.ofHours(1)),
+    MINUTE_5(Duration.ofMinutes(5)),
+    MINUTE_1(Duration.ofMinutes(1)),
     ;
 
-    private final String name;
     private final Duration duration;
 
     @Override
     public String toString() {
-        return this.name;
+        return this.name();
     }
 
     public long getMilliseconds() {
